ba5bf5e4822f89ed13a320a5ba262bf9ab458325
Ashish sisodia <ashish.sisodia@appinventiv.com>
2022-08-24 17:05:47 +0530

    add framework

---
+++ AdvanceFramework/AdvanceFramework/AdvanceFramework.h
@@ -0,0 +1,18 @@
+//
+//  AdvanceFramework.h
+//  AdvanceFramework
+//
+//  Created by Admin on 24/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvanceFramework.
+FOUNDATION_EXPORT double AdvanceFrameworkVersionNumber;
+
+//! Project version string for AdvanceFramework.
+FOUNDATION_EXPORT const unsigned char AdvanceFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvanceFramework/PublicHeader.h>
+
+
---
+++ AdvanceFramework/AdvanceFramework/Validator.swift
@@ -0,0 +1,17 @@
+//
+//  Validator.swift
+//  AdvanceFramework
+//
+//  Created by Admin on 24/08/22.
+//
+
+import Foundation
+ 
+
+public struct Validator {
+    
+    public static func validateEmail() {
+        
+    }
+    
+}
